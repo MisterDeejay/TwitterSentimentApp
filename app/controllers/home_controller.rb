40717877c8d9a85bd50ba005169a86669b923fc8
Khaaliq DeJan <khaaliqdejan@gmail.com>
2015-05-13 13:18:54 -0700

    add tweets model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   def root; end
 
   def get_tweets
-    @tweets = $twitter.search(query_params, count: 100)
+    @tweets = $twitter.search(query_params, count: 1)
+    binding.pry
     render json: @tweets
   end
 
