1019002cc2de47a0def1e4b1d43102c40bcfbd3c
Khaaliq DeJan <khaaliqdejan@gmail.com>
2015-05-20 04:21:00 -0700

    add backbone compositeView

--- app/assets/javascripts/tweetApp.js
+++ app/assets/javascripts/tweetApp.js
@@ -8,3 +8,21 @@ window.TweetApp = {
     $("body").append(view.render().$el);
   }
 };
+
+
+Backbone.CompositeView = Backbone.View.extend({
+  addSubview: function(selecter, subview) {
+    this.subviews(selector).push(subview);
+  },
+
+  subviews: function(selector) {
+    this._subviews = this._subview || {}
+    
+    if(!selector) {
+      return this._subviews;
+    } else {
+      this._subviews[selector] = this._subviews[selector] || [];
+      return this._subviews[selector];
+    }
+  },
+});
