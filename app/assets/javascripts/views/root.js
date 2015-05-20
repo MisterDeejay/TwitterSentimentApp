e15aa4f9c1cd532b4c00bc698ba2214b7187e6c1
Khaaliq DeJan <khaaliqdejan@gmail.com>
2015-05-20 04:23:01 -0700

    root view NOW extends Backbone.CompositeView

--- app/assets/javascripts/views/root.js
+++ app/assets/javascripts/views/root.js
@@ -1,4 +1,4 @@
-window.TweetApp.Views.Root = Backbone.View.extend({
+window.TweetApp.Views.Root = Backbone.CompositeView.extend({
   template: JST["root"],
 
   events: {
@@ -10,15 +10,20 @@ window.TweetApp.Views.Root = Backbone.View.extend({
     this.addPageLoad();
 
     var queryStr = $("form").serializeJSON();
+    var tweets = [];
+    debugger
     $.ajax({
+      context: this,
       url: '/get_tweets',
       type: 'POST',
       data: queryStr,
       success: function(resp) {
-        debugger
-        alert("Success!");
+        this.collection = TweetApp.Collections.Tweets(resp);
+        this.removePageLoad();
+
+        this.render();
       },
-      error: function() {
+      error: function(resp) {
         alert("Dafuq!?!!?");
       }
     });
@@ -31,7 +36,23 @@ window.TweetApp.Views.Root = Backbone.View.extend({
     $(".tweets-container").html($p);
   },
 
+  addTweetSubview: function(tweet) {
+    var tweetView = new TweetApp.Views.TweetView({
+      model: tweet
+    });
+    this.addSubview("ul.tweet-index", tweetView);
+    // Stuff TO-DO:
+    // 1) add a ul to the template
+    // 2) make a new TweetApp.Views.TweetView class
+  },
+
   render: function() {
+    var view = this;
+    if (typeof this.collection != 'undefined') {
+      this.collection.each(function(tweet) {
+        view.addTweetSubview(tweet);
+      });
+    }
     var content = this.template();
     this.$el.html(content);
 
